836c8ae365fcb905a83232fb0fbd4c453fea6962
PanValeriy <64436706+PanValeriy@users.noreply.github.com>
2020-04-29 07:58:57 +0300

    Add files via upload

--- Unit1.py
+++ Unit1.py
@@ -1,5 +1,17 @@
-x = int(input('x = '))
-if x<0 :
-	print('x < 0')
-else:
-	print('...')
+import turtle
+
+pen = turtle.Pen()
+
+def speed(d):
+   pen.fd(d)
+   pen.rt(5)
+
+d = 0
+gradus = 180
+while gradus>0:
+	d=d+0.1
+	speed(d)
+	gradus-=1
+
+
+input()
